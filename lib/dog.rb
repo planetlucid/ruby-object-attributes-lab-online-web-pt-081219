6af6b9907a685da9b1a601884f20b6c787193d0d
Ryan S <planetlucid@gmail.com>
2019-08-29 17:39:12 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+ 
+ def name=(names)
+    @name = names
+  end
+ 
+  def name
+    @name
+  end
+  
+  def breed=(breeds)
+    @breed = breeds
+  end
+ 
+  def breed
+    @breed 
+  end
+  
+end
+
